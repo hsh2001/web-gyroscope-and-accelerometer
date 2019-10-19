d4d8f5fd1c1fa98688662483185ad048069561ef
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2019-10-19 13:01:42 +0900

    ADD ball

--- m.js
+++ m.js
@@ -1,17 +1,35 @@
+// Promise.all(
+//   [
+//     DeviceMotionEvent,
+//     DeviceOrientationEvent,
+//   ].map(
+//     eve => eve.requestPermission()
+//   )
+// ).then(permissions => {
+//   permissions.map(alert);
+// });
+
+const ball = document.getElementById('ball');
+
 window.addEventListener('deviceorientation', event => {
+  const dx = event.beta / 300 + parseFloat(ball.style.top);
+  const dy = event.gamma / 300 + parseFloat(ball.style.left);
+
   [
-    'absolute',
     'alpha',
     'beta',
     'gamma',
   ].forEach(key => {
     document.getElementById(key).innerHTML = event[key];
   });
+
+  ball.style.top = `${dx.toFixed(2)}px`;
+  ball.style.left = `${dy.toFixed(2)}px`;
 }, true);
 
 window.addEventListener('devicemotion', event => {
   const acc = event.accelerationIncludingGravity;
-  Array.from("xyzr").forEach(key => {
+  Array.from("xyz").forEach(key => {
     document.getElementById(key).innerHTML = acc[key];
   });
 }, true);
